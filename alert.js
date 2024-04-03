22349a008111f23d8990ad9095af3c7ff095cce8
RamaRamdani03 <ramaramdani449@gamil.com>
2024-04-04 02:09:07 +0700

    firstcommit

---
+++ alert.js
@@ -0,0 +1,15 @@
+function showAlert() {
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    Document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil() {
+        alert("sayang sama aku ga? kalo sayang langsung chat aku 'Love U' boleh ga>")
+    }
+
+    function show() {
+        alert(" Love u sengg 😙")
+    }
+}
